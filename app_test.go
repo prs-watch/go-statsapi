3f2fe5c5f756fd6deae8581355bf29aa0d585e44
miyazaki <scarsdaleny15@yahoo.co.jp>
2020-01-02 13:07:20 +0900

    ~update add functions

--- app_test.go
+++ app_test.go
@@ -8,6 +8,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Util function to read answer JSON.
+func readAnswer(filePath string) (body string) {
+	file, _ := os.Open(filePath)
+	bodyBuf, _ := ioutil.ReadAll(file)
+	return string(bodyBuf)
+}
+
 func TestAttendance(t *testing.T) {
 	params := new(Params)
 	params.TeamID = "143"
@@ -69,9 +76,64 @@ func TestBoxScore(t *testing.T) {
 	assert.JSONEq(t, result, ans)
 }
 
-// Util function to read answer JSON.
-func readAnswer(filePath string) (body string) {
-	file, _ := os.Open(filePath)
-	bodyBuf, _ := ioutil.ReadAll(file)
-	return string(bodyBuf)
+func TestLineScore(t *testing.T) {
+	params := new(Params)
+	result, _ := LineScore("529572", params)
+
+	ans := readAnswer("testdata/linescore_ans.json")
+
+	assert.JSONEq(t, result, ans)
+}
+
+// Timestamp assertion issue exists!
+// func TestStandings(t *testing.T) {
+// 	params := new(Params)
+// 	params.Season = "2019"
+// 	params.LeagueID = "103"
+// 	result, _ := Standings(params)
+
+// 	ans := readAnswer("testdata/standings_ans.json")
+
+// 	assert.JSONEq(t, result, ans)
+// }
+
+func TestTeamLeaders(t *testing.T) {
+	params := new(Params)
+	params.Season = "2019"
+	params.LeaderCategories = "earnedRunAverage"
+	result, _ := TeamLeaders("143", params)
+
+	ans := readAnswer("testdata/team_leaders_ans.json")
+
+	assert.JSONEq(t, result, ans)
+}
+
+func TestLeagueLeaders(t *testing.T) {
+	params := new(Params)
+	params.Season = "2019"
+	params.LeaderCategories = "earnedRunAverage"
+	params.PlayerPool = "qualified"
+	result, _ := LeagueLeaders(params)
+
+	ans := readAnswer("testdata/league_leaders_ans.json")
+
+	assert.JSONEq(t, result, ans)
+}
+
+func TestPlayerInfo(t *testing.T) {
+	params := new(Params)
+	result, _ := PlayerInfo("547943", params)
+
+	ans := readAnswer("testdata/player_info_ans.json")
+
+	assert.JSONEq(t, result, ans)
+}
+
+func TestPlayByPlay(t *testing.T) {
+	params := new(Params)
+	result, _ := PlayByPlay("529572", params)
+
+	ans := readAnswer("testdata/play_by_play_ans.json")
+
+	assert.JSONEq(t, result, ans)
 }
